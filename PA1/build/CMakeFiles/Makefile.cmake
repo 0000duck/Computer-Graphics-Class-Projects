deac23022b18f2f8546df35148ce167e3e3f0a41
Grant Hooks <grant.hooks@ymail.com>
2017-09-06 10:55:07 -0700

    README, build folder

--- PA1/build/CMakeFiles/Makefile.cmake
+++ PA1/build/CMakeFiles/Makefile.cmake
@@ -52,6 +52,6 @@ SET(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 SET(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/Tutorial.dir/DependInfo.cmake"
-  "CMakeFiles/Tutorial_SUCCESSFUL.dir/DependInfo.cmake"
+  "CMakeFiles/PA1.dir/DependInfo.cmake"
+  "CMakeFiles/PA1_SUCCESSFUL.dir/DependInfo.cmake"
   )
